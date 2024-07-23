31af564a3248fda90dff4b1eb93f38d51e72d355
Prasai1488 <prasaiprithvi4@gmail.com>
2024-07-23 16:34:36 +0545

    authContext

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./card.scss";
 import apiRequest from "../../lib/apiRequest";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function Card({ item }) {
   const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
 
   const handleEdit = () => {
     navigate(`/edit/${item.id}`);
@@ -44,18 +47,22 @@ function Card({ item }) {
             </div>
           </div>
           <div className="icons">
-            <button className="icon" onClick={handleEdit}>
-              <img
-                src="https://cdn3.iconfinder.com/data/icons/feather-5/24/edit-512.png"
-                alt="Edit"
-              />
-            </button>
-            <button className="icon" onClick={handleDelete}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQ8KY2jPdcbKF3O-3yLkr452rUd4TN8Uaxg&s"
-                alt="Delete"
-              />
-            </button>
+            {currentUser && currentUser.id === item.userId && (
+              <>
+                <button className="icon" onClick={handleEdit}>
+                  <img
+                    src="https://cdn3.iconfinder.com/data/icons/feather-5/24/edit-512.png"
+                    alt="Edit"
+                  />
+                </button>
+                <button className="icon" onClick={handleDelete}>
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQ8KY2jPdcbKF3O-3yLkr452rUd4TN8Uaxg&s"
+                    alt="Delete"
+                  />
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -64,3 +71,4 @@ function Card({ item }) {
 }
 
 export default Card;
+
